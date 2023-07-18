892c2c348402c9112b2f6f6e849ece00477dab8c
WaleedAljaberi <139317678+WaleedAljaberi@users.noreply.github.com>
2023-07-18 13:15:01 +0400

    Update start_sim.sh

--- start_sim.sh
+++ start_sim.sh
@@ -2,4 +2,4 @@
 
 source $HOME/mbzirc_ws/install/setup.bash
 
-ros2 launch mbzirc_ros competition_local.launch.py ign_args:="-v 4 -r coast.sdf" & ros2 launch mbzirc_ign spawn.launch.py name:=usv world:=coast model:=usv x:=-1462 y:=-16.5 z:=0.3 R:=0 P:=0 Y:=0 && fg
+ros2 launch mbzirc_ros competition_local.launch.py ign_args:="-v 4 -r coast.sdf" & ros2 launch mbzirc_ign spawn.launch.py name:=usv world:=coast model:=usv x:=-1450 y:=-16.5 z:=0.3 R:=0 P:=0 Y:=0 && fg
